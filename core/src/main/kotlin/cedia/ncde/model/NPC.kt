1121525cab4b3dfafbf51a75e28cdfc7d6b29dbb
unknown <fxrob@none.com>
2016-05-24 22:59:28 -0400

    Set default combat definition property 'followClose' to true

--- core/src/main/kotlin/cedia/ncde/model/NPC.kt
+++ core/src/main/kotlin/cedia/ncde/model/NPC.kt
@@ -5,9 +5,8 @@ import cedia.ncde.io.store.NPCNamesStore
 /**
  * Created on 2016-05-22.
  */
-data class NPC(val id: Int, val name: String = NPCNamesStore.getNameForId(id),
-               var combatDefinition: CombatDefinition =
-               CombatDefinition(0, -1, -1, -1, 0, -1, -1, 0.0, false, false, false, false, 1)) {
+data class NPC(val id: Int, val name: String = NPCNamesStore.getNameForId(id), var combatDefinition: CombatDefinition =
+CombatDefinition(0, -1, -1, -1, 0, -1, -1, 0.0, true, false, false, false, 1)) {
 
     override fun toString(): String {
         return "[$id] $name"
